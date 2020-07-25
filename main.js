1726ca4730ad11fae300386806f900473fd066c2
ivesklein <dvdklein@gmail.com>
2020-07-25 15:46:38 -0400

    loop

--- main.js
+++ main.js
@@ -1,15 +1,25 @@
 const yi = require('yi-action-camera');
 
+const sleep = function(milis){
+	return new Promise((resolve, reject) => {
+		setTimeout(() => {
+			resolve()
+		}, milis)
+	})
+}
 
 const main = async function(){
 
 	try{
+		console.log('Starting yi-timelapse')
 		await yi.connect()
 		console.log('connected')
-		const filepath = await yi.takePhoto()
-		console.log({filepath})
-		const config = await yi.getConfig()
-		console.log({config})
+		
+		for(let i in [0,1,2,3,4,5,6,7,8,9]){
+			const filepath = await yi.takePhoto()
+			console.log({i,filepath})
+			await sleep(5000)
+		}
 	}catch(e){
 		console.log(e)
 	}
